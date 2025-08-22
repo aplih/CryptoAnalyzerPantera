8be487004683e768cfa8a1d64bf7ab5c8cb1d7b2
Ilya T. <zver568@gmail.com>
2025-08-22 10:34:40 +0300

    ADD MainApp
    Clarifying the encryption request, waiting fir keyboard input

---
+++ src/main/java/com/javarush/toporov/MainApp.java
@@ -0,0 +1,11 @@
+package com.javarush.toporov;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Choose an encryption, method: \n 1. Encrypt \n 2. Decrypt");
+        Scanner scanner = new Scanner(System.in);
+        String console = scanner.nextLine();
+    }
+}
